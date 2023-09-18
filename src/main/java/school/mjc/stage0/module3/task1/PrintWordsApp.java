1282a53f084d2f516e7006151e0f1433ec95604a
Bakhtiyar Kussainov <bakhtiyar_kussainov@epam.com>
2023-09-18 13:45:53 +0500

    init

--- src/main/java/school/mjc/stage0/module3/task1/PrintWordsApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/PrintWordsApp.java
@@ -2,6 +2,7 @@ package school.mjc.stage0.module3.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        //write your code here
+        String phrase = "java\nis\nsmth";
+        System.out.println(phrase);
     }
 }
